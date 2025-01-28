b8c717964287e10587187e2c939b671ab3dc181f
mustafa <i.mustafahk@gmail.com>
2025-01-28 19:45:16 +0500

    added hardcoded endpoints for backup registration

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import AttendanceForm from './components/AttendanceForm';
 import BackupAttendanceForm from './components/BackupAttendanceForm';
 import './App.css';
+
+// Unique hashed endpoint for backup form
+const BACKUP_HASH = "8f3da5b1e6c4d2a9"; // This is just an example hash
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <nav className="nav-links">
-          <Link to="/">Attendance</Link>
-          <Link to="/backup">Backup Attendance</Link>
-        </nav>
-        
         <Routes>
           <Route path="/" element={<AttendanceForm />} />
-          <Route path="/backup" element={<BackupAttendanceForm />} />
+          <Route path={`/backup/${BACKUP_HASH}`} element={<BackupAttendanceForm />} />
+          {/* Redirect any /backup attempts without hash */}
+          <Route path="/backup" element={<Navigate to="/" replace />} />
         </Routes>
       </div>
     </Router>
